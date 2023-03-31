4d2791d28a65b207cd201a47c4166a141eaaedc6
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-03-31 14:56:36 +0200

    fixes to the database

--- Database/face.io.session.sql
+++ Database/face.io.session.sql
@@ -3,9 +3,7 @@ DROP TABLE IF EXISTS emp_info;
 CREATE TABLE emp_info(
 empl_no INT PRIMARY KEY NOT NULL,
 full_name VARCHAR(50) NOT NULL,
-surname VARCHAR(50) NOT NULL,
-cohort VARCHAR(25) NOT NULL,
-event_time TIMESTAMP   
+cohort VARCHAR(25) NOT NULL
 );
 
 

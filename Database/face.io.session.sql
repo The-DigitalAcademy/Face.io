6f45d8a42546a780fc049fb7ea6e4aa198cf70cc
johhnymagome24@gmail.com <johhnymagome24@gmail.com>
2023-04-19 15:09:19 +0200

    added changes

--- Database/face.io.session.sql
+++ Database/face.io.session.sql
@@ -6,13 +6,7 @@ full_name VARCHAR(50) NOT NULL,
 cohort VARCHAR(25) NOT NULL
 );
 
-DROP TABLE IF EXISTS face_embeddings;
 
-CREATE TABLE face_embeddings(
-empl_no INT PRIMARY KEY NOT NULL,
-embeddings FLOAT[] NOT NULL,
-embeddings_type VARCHAR(50) NOT NULL
-);
 
 
    
